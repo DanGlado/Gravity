13a5e5f8f88183b9fcf451db19cc12bb6dc73e99
202530 <dgladushenko@mail.ru>
2021-12-21 19:10:03 +0300

    Test Success

--- app/src/main/java/com/example/gravity/scenes/MainMenuScene.java
+++ app/src/main/java/com/example/gravity/scenes/MainMenuScene.java
@@ -23,11 +23,11 @@ public class MainMenuScene extends SceneFW{
     @Override
     public void drawing() {
         graphicsFW.clearScene(Color.BLACK);
-        graphicsFW.drawText(coreFW.getString(R.string.txt_mainMenu_nameGame), 200, 100, Color.BLUE, 60, null);
-        graphicsFW.drawText(coreFW.getString(R.string.txt_mainMenu_newGame), 50, 300, Color.BLUE, 40, null);
-        graphicsFW.drawText(coreFW.getString(R.string.txt_mainMenu_settings), 50, 350, Color.BLUE, 40, null);
-        graphicsFW.drawText(coreFW.getString(R.string.txt_mainMenu_leaders), 50, 400, Color.BLUE, 40, null);
-        graphicsFW.drawText(coreFW.getString(R.string.txt_mainMenu_exitGame), 50, 450, Color.BLUE, 40, null);
+        graphicsFW.drawText(coreFW.getString(R.string.txt_mainMenu_nameGame), 200, 100, Color.BLUE, 70, null);
+        graphicsFW.drawText(coreFW.getString(R.string.txt_mainMenu_newGame), 40, 300, Color.BLUE, 50, null);
+        graphicsFW.drawText(coreFW.getString(R.string.txt_mainMenu_settings), 40, 400, Color.BLUE, 50, null);
+        graphicsFW.drawText(coreFW.getString(R.string.txt_mainMenu_leaders), 40, 450, Color.BLUE, 50, null);
+        graphicsFW.drawText(coreFW.getString(R.string.txt_mainMenu_exitGame), 40, 500, Color.BLUE, 50, null);
     }
 
     @Override
